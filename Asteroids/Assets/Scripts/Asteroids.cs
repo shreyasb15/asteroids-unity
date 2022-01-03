ae6377e48c47508a48f91f61de0a85c7c722318e
Shreyas Borakhadikar <shreyasb1511@gmail.com>
2022-01-03 18:36:28 +0530

    spawning asteroids

--- Asteroids/Assets/Scripts/Asteroids.cs
+++ Asteroids/Assets/Scripts/Asteroids.cs
@@ -10,6 +10,9 @@ public class Asteroids : MonoBehaviour
     public float minSize = 0.5f;
     public float maxSize = 1.5f;
 
+    public float speed = 50f;
+    public float maxLifeTime = 30f;
+
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
 
@@ -27,5 +30,12 @@ public class Asteroids : MonoBehaviour
         this.transform.localScale = Vector3.one * this.size;
 
         rb.mass = this.size;
+    }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        rb.AddForce(direction * this.speed);
+
+        Destroy(this.gameObject, this.maxLifeTime);
     }
 }
